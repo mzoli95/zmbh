73d80c1541909fd6994971b11e1bcb04617d368a
MZoli <zoli19950714@gmail.com>
2023-12-10 19:06:35 +0100

    about component routing fix

--- zmbh-fe/src/app/app-routing.module.ts
+++ zmbh-fe/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './layout/zmbh-portfolio/home/home.component';
+import { AboutComponent } from './layout/zmbh-portfolio/about/about.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
-  {path:'home', redirectTo: ''}
+  {path:'home', redirectTo: ''},
+  {path:'about', component:AboutComponent}
 ];
 
 @NgModule({
