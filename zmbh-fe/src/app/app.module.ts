94a96d8497ecdf88a3cddc6b798d210829413ea4
Balage1234 <bazsi430@gmail.com>
2023-12-11 18:49:12 +0100

    add sticky footer and about component

--- zmbh-fe/src/app/app.module.ts
+++ zmbh-fe/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,23 +11,12 @@ import { ButtonModule } from 'primeng/button';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './layout/zmbh-portfolio/home/home.component';
 import { AboutComponent } from './layout/zmbh-portfolio/about/about.component';
+import { FooterComponent } from './layout/shared/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ButtonModule,
-    NgbModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule, NgbModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
