70a8db32c1785d99a3c656187c7ea1ca691c25b0
MZoli <zoli19950714@gmail.com>
2023-12-13 18:37:43 +0100

    change primeng to materials, ui fixes

--- zmbh-fe/src/app/app.module.ts
+++ zmbh-fe/src/app/app.module.ts
@@ -7,16 +7,28 @@ import {
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/shared/header/header.component';
-import { ButtonModule } from 'primeng/button';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './layout/zmbh-portfolio/home/home.component';
 import { AboutComponent } from './layout/zmbh-portfolio/about/about.component';
 import { FooterComponent } from './layout/shared/footer/footer.component';
-import { CardModule } from 'primeng/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, ButtonModule, NgbModule,CardModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+  ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
