9b320d30d421dcbf86dc391817da573084256444
MZoli <zoli19950714@gmail.com>
2023-12-17 21:05:27 +0100

    about - about us

---
+++ zmbh-fe/src/app/layout/zmbh-portfolio/about/connect/connect.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zmbh-about-connect',
+  templateUrl: './connect.component.html',
+  styleUrl: './connect.component.scss',
+})
+export class ConnectComponent {}
--- zmbh-fe/src/app/layout/zmbh-portfolio/zmbh.module.ts
+++ zmbh-fe/src/app/layout/zmbh-portfolio/zmbh.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { MatCardModule } from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import { AboutUsComponent } from './about/about-us/about-us.component';
+import { ConnectComponent } from './about/connect/connect.component';
+import { TeamComponent } from './about/team/team.component';
 
 @NgModule({
-  declarations: [HomeComponent, AboutComponent],
-  imports: [MatCardModule],
+  declarations: [HomeComponent, AboutComponent, AboutUsComponent, ConnectComponent,TeamComponent],
+  imports: [MatCardModule,MatTabsModule],
 })
 export class ZmbhModule {}
