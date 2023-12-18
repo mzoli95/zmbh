b4478958343b9299474f121dae92fdb546380566
MZoli <zoli19950714@gmail.com>
2023-12-18 12:42:34 +0100

    contact routing from home, design todos

--- zmbh-fe/src/app/layout/zmbh-portfolio/zmbh.module.ts
+++ zmbh-fe/src/app/layout/zmbh-portfolio/zmbh.module.ts
@@ -6,9 +6,10 @@ import {MatTabsModule} from '@angular/material/tabs';
 import { AboutUsComponent } from './about/about-us/about-us.component';
 import { GoalComponent } from './about/goal/goal.component';
 import { TeamComponent } from './about/team/team.component';
+import { ZmbhRoutingModule } from './zmbh-routing.component';
 
 @NgModule({
   declarations: [HomeComponent, AboutComponent, AboutUsComponent, GoalComponent,TeamComponent],
-  imports: [MatCardModule,MatTabsModule],
+  imports: [MatCardModule,MatTabsModule,ZmbhRoutingModule],
 })
 export class ZmbhModule {}
