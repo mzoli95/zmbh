4d22bf81ac8b0408e81738dd08e26fcc7c6db529
Balage1234 <bazsi430@gmail.com>
2024-01-16 14:46:58 +0100

    carousel make dynamic, and added a mockdb.ts model which contains the details of the project's pictures

---
+++ zmbh-fe/src/app/layout/shared/mockdb.ts
@@ -0,0 +1,39 @@
+export class PictureModel {
+  pictures = [
+    {
+      imgPath: '../../../../assets/innovate2.png',
+      projectTitle: 'Project 1',
+      projectAlt: 'Project 1',
+      projectDetail:
+        'Aliquam erat volutpat. Proin tincidunt sagittis libero, vel rhoncus dolor elementum vulputate. Aenean sollicitudin libero malesuada commodo vestibulum. Vestibulum ac dolor gravida, molestie purus ac, aliquet enim. Phasellus auctor mi sed imperdiet ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis tortor lacinia, auctor massa non, varius elit. Aliquam accumsan nunc nec nulla fringilla fringilla.',
+    },
+    {
+      imgPath: '../../../../assets/innovate4.png',
+      projectTitle: 'Project 2',
+      projectAlt: 'Project 2',
+      projectDetail:
+        'Aliquam erat volutpat. Proin tincidunt sagittis libero, vel rhoncus dolor elementum vulputate. Aenean sollicitudin libero malesuada commodo vestibulum. Vestibulum ac dolor gravida, molestie purus ac, aliquet enim. Phasellus auctor mi sed imperdiet ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis tortor lacinia, auctor massa non, varius elit. Aliquam accumsan nunc nec nulla fringilla fringilla.',
+    },
+    {
+      imgPath: '../../../../assets/innovate2.png',
+      projectTitle: 'Project 3',
+      projectAlt: 'Project 3',
+      projectDetail:
+        'Aliquam erat volutpat. Proin tincidunt sagittis libero, vel rhoncus dolor elementum vulputate. Aenean sollicitudin libero malesuada commodo vestibulum. Vestibulum ac dolor gravida, molestie purus ac, aliquet enim. Phasellus auctor mi sed imperdiet ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis tortor lacinia, auctor massa non, varius elit. Aliquam accumsan nunc nec nulla fringilla fringilla.',
+    },
+    {
+      imgPath: '../../../../assets/innovate4.png',
+      projectTitle: 'Project 4',
+      projectAlt: 'Project 4',
+      projectDetail:
+        'Aliquam erat volutpat. Proin tincidunt sagittis libero, vel rhoncus dolor elementum vulputate. Aenean sollicitudin libero malesuada commodo vestibulum. Vestibulum ac dolor gravida, molestie purus ac, aliquet enim. Phasellus auctor mi sed imperdiet ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis tortor lacinia, auctor massa non, varius elit. Aliquam accumsan nunc nec nulla fringilla fringilla.',
+    },
+    {
+      imgPath: '../../../../assets/innovate2.png',
+      projectTitle: 'Project 5',
+      projectAlt: 'Project 5',
+      projectDetail:
+        'Aliquam erat volutpat. Proin tincidunt sagittis libero, vel rhoncus dolor elementum vulputate. Aenean sollicitudin libero malesuada commodo vestibulum. Vestibulum ac dolor gravida, molestie purus ac, aliquet enim. Phasellus auctor mi sed imperdiet ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis tortor lacinia, auctor massa non, varius elit. Aliquam accumsan nunc nec nulla fringilla fringilla.',
+    },
+  ];
+}
--- zmbh-fe/src/app/layout/zmbh-portfolio/home/portfolio/portfolios.component.ts
+++ zmbh-fe/src/app/layout/zmbh-portfolio/home/portfolio/portfolios.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PictureModel } from '../../../shared/mockdb';
 
 @Component({
   selector: 'app-mzbh-home-portfolios',
@@ -6,31 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './portfolios.component.scss',
 })
 export class HomePortfoliosComponent {
-  pictures = [
-    {
-      url: '../../../../assets/innovate2.png',
-      title: 'Project 1',
-      alt: 'Project 1',
-    },
-    {
-      url: '../../../../assets/innovate4.png',
-      title: 'Project 2',
-      alt: 'Project 2',
-    },
-    {
-      url: '../../../../assets/innovate2.png',
-      title: 'Project 3',
-      alt: 'Project 3',
-    },
-    {
-      url: '../../../../assets/innovate4.png',
-      title: 'Project 4',
-      alt: 'Project 4',
-    },
-    {
-      url: '../../../../assets/innovate2.png',
-      title: 'Project 5',
-      alt: 'Project 5',
-    },
-  ];
+  pictures: any;
+
+  constructor() {
+    let pictureModel = new PictureModel();
+    this.pictures = pictureModel.pictures;
+  }
 }
